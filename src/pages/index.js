d367a48b1e92a611e86850c34761f728d1815e4a
ys <ys@sina.com>
2024-01-17 13:49:51 +0800

    frist

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import {TextSimiler} from '../components/component/text-similer'
+
+
+export default function Home() {
+  return (
+    <div>
+      <TextSimiler />
+    </div>
+  )
+}
---
+++ src/pages/statis.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import {TextSimiler} from '../components/component/text-similer'
+
+//统计页面
+export default function Home() {
+  return (
+    <div>
+      <TextSimiler />
+    </div>
+  )
+}
