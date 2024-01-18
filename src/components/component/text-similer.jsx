d4a006587b44d5aa176de120f3ba15515489e3d0
ys <ys@sina.com>
2024-01-18 16:47:11 +0800

    优化提示

--- src/components/component/text-similer.jsx
+++ src/components/component/text-similer.jsx
@@ -40,9 +40,10 @@ export function TextSimiler() {
       className="flex flex-col min-h-screen p-4 md:p-8 bg-gradient-to-r from-[#f3f4f6] to-[#a1c9f1] text-[#2d3748] font-sans">
      <NavBar />
      <div className="flex flex-col">
-        <select onChange={(e) => setTextVector(e.target.value)} className="mb-6 font-bold bg-gradient-to-r from-[#f3f4f6] to-[#a1c9f1] w-32 h-8" >
-          <option className="mb-4 w-2 "  value={1}>平均向量(适合句子)</option>
-          <option className="mb-4  "  value={2}>TF-IDF向量(适合文章)</option>
+        <select onChange={(e) => setTextVector(e.target.value)} className="mb-6 font-bold bg-gradient-to-r from-[#f3f4f6] to-[#a1c9f1] w-40 h-8" >
+          <option className="mb-4 " value={1} >选择文本向量算法</option>
+          <option className="mb-4 "  value={1}>平均向量(适合句子)</option>
+          <option className="mb-4 "  value={2}>TF-IDF向量(适合文章)</option>
         </select>
     </div>
       <div className="flex flex-col md:grid md:grid-cols-2 gap-4 flex-grow">
