948df1b1bbafe9bd40eb776a3a2aa31028d2c710
ys <ys@sina.com>
2024-01-19 18:30:27 +0800

    词向量相似度api

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -68,3 +68,18 @@ try {
     throw error; // 可以选择在这里抛出错误，也可以返回一个默认值或其他处理方式
   }
 };
+
+export const wordSimilarity = async (words) => {
+  // console.log("rext1: "+text1+"\n text2: "+text2)
+  // return 99.443322;
+try {
+  const response = await axios.post(`${API_BASE_URL}/word-similarity`, {
+    words: words,
+   });
+  console.log(response)
+  return response.data;
+  } catch (error) {
+    console.error('请求失败：', error);
+    throw error; // 可以选择在这里抛出错误，也可以返回一个默认值或其他处理方式
+  }
+};
