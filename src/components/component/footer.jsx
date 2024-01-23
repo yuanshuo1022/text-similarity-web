c5ca89632080cee3569326788e75223c5d38911a
ys <ys@sina.com>
2024-01-23 09:33:23 +0800

    metamask

--- src/components/component/footer.jsx
+++ src/components/component/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="bg-gray-200 p-4 text-center">
       <div className="text-sm">
-       <Link  href="/gapword"> ICP备案号：Your ICP备案号</Link>
+       <Link  href="https://www.baidu.com/"> ICP备案号：Your ICP备案号</Link>
         <p>© 2024 Your Company. All rights reserved.</p>
       </div>
     </footer>
